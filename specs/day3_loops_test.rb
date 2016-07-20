927cd719d887d0c6388cba8d06b47d3a09c36a1f
Michael Snaddon <msnaddon0991@gmail.com>
2016-07-20 14:20:29 +0100

    Test for array function complete

--- specs/day3_loops_test.rb
+++ specs/day3_loops_test.rb
@@ -6,9 +6,16 @@ require_relative( '../day3_loops.rb' )
 class Functions_Practice < MiniTest::Test
 
   def test_add_size_of_two_arrays()
+    prices = [ 1.23, 6.98, 8.43, 2.45 ]
+    costs = [ 4.23, 1.12, 0.52, 8.67 ]
+    result1 = size_of_two_arrays(prices, costs)
+    assert_equals(8,result1)
 
   end
-  def test_add_
+  # def test_add_sum_of_array()
+
+  # end
+  # def test_
 
 
 
